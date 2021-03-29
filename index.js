d0c99e18c10a69333456a72b17c87e45ae7231b1
Rutger Weemhoff <rutger.weemhoff@ticketmatic.nl>
2021-03-29 21:49:52 +0200

    Index

--- index.js
+++ index.js
@@ -1,28 +1,4 @@
 "use strict"
 
-var env = require('node-env-file')
-
-env(__dirname + '/.env')
-
-const contacts = require('./lib/contacts')
-
-const client = {
-	shortname: process.env.SHORTNAME,
-	key: process.env.API_KEY,
-	secret: process.env.API_SECRET,
-    language: process.env.LANGUAGE ?? 'en',
-}
-
-async function main() {
-    try {
-        //const res = await contacts.getCreditInfo(client, 30104)
-        //const res = await contacts.getTicketInfo(client, 30104)
-        const res = await contacts.getPricetypeVoucherInfo(client, 30104)
-        console.log(res)
-    }
-    catch (err) {
-        console.log(err)
-    }
-}
-
-main()
+exports.contacts = require('./lib/contacts')
+exports.orders = require('./lib/orders')
