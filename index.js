50ac4a810ad7d409a5507c521ec8cc5c0da7c215
Rutger Weemhoff <rutger@direct-control.nl>
2021-09-14 11:40:57 +0200

    Add optins

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 "use strict"
 
 exports.contacts = require('./lib/contacts')
+exports.optins = require('./lib/optins')
 exports.orders = require('./lib/orders')
 exports.ordercosts = require('./lib/ordercosts')
 exports.waitinglist = require('./lib/waitinglist')
